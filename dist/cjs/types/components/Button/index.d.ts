38246b35125156df7e45f1f389a98df3f11ae248
JangAyeon <mediarkayeon@gmail.com>
2024-11-27 08:36:43 +0900

    feat: add precommit script

--- dist/cjs/types/components/Button/index.d.ts
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { type ButtonProps } from "./types";
-declare const Button: React.FC<ButtonProps>;
-export default Button;
--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface ButtonProps {
-    content: string;
-}
-
-declare const Button: React.FC<ButtonProps>;
-
-export { Button };

f9a4dee470892410781a103e6d5c0584943ee53b
JangAyeon <mediarkayeon@gmail.com>
2024-11-26 09:14:20 +0900

    feat: add husky pre-commit test

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,16 +1,14 @@
-import React from "react";
-import { Meta, StoryObj } from "@storybook/react";
+import { type Meta, type StoryObj } from "@storybook/react";
 
 import Button from "./index";
-import { type ButtonProps } from "./types";
 
 const meta: Meta<typeof Button> = {
   title: "Components/Button", // Storybook 내에서 컴포넌트가 표시될 경로
   component: Button,
   tags: ["autodocs"], // 자동으로 문서를 생성하는 태그
   argTypes: {
-    content: { control: "text", description: "Button text content" }, // 컨트롤 추가
-  },
+    content: { control: "text", description: "Button text content" } // 컨트롤 추가
+  }
 };
 
 export default meta;
@@ -19,18 +17,18 @@ type Story = StoryObj<typeof Button>;
 
 export const Default: Story = {
   args: {
-    content: "Click Me!", // 기본 props 설정
-  },
+    content: "Click Me!" // 기본 props 설정
+  }
 };
 
 export const Primary: Story = {
   args: {
-    content: "Primary Button",
-  },
+    content: "Primary Button"
+  }
 };
 
 export const WithLongText: Story = {
   args: {
-    content: "This is a button with a longer text",
-  },
+    content: "This is a button with a longer text"
+  }
 };

461e40d3f4d2a15ef52fd98fa57e1f2c49d286e4
JangAyeon <ayeon3052@ewhain.net>
2024-12-29 03:07:44 +0900

    feat: add modal Component

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,16 @@
+import Content from "./Layout/content";
+import Footer from "./Layout/footer";
+import Header from "./Layout/header";
+import Wrapper from "./Layout/wrapper";
+
+export const Modal = Object.assign(
+  {},
+  {
+    Wrapper,
+    Header,
+    Content,
+    Footer,
+  }
+);
+
+export default Modal;

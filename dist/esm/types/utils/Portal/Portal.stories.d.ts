c33ca8e29f920d072dfa32bfa6bc05f28de12993
JangAyeon <ayeon3052@ewhain.net>
2025-01-04 16:29:58 +0900

    fix: change git ignore

---
+++ dist/esm/types/utils/Portal/Portal.stories.d.ts
@@ -0,0 +1,7 @@
+import type { Meta } from "@storybook/react";
+import { Portal } from ".";
+declare const meta: Meta<typeof Portal>;
+export default meta;
+export declare const InContainer: () => JSX.Element;
+export declare const NoContainer: () => JSX.Element;
+export declare const withBackdropControl: () => JSX.Element;
